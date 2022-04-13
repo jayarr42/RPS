d455adfde56031dc53fea4fd28260c00a50d319d
JayArr <jrollock57@gmail.com>
2022-04-12 21:39:35 -0400

    Add variable computerSelection to set variables for computerPlay

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 // user input
-let playerSelection = prompt ("What is your choice?");
+//let playerSelection = prompt ("What is your choice?");
 
-let computerSelection = Math.random();
+// assign variable to the comp select, used in comp play
+let computerSelection = Math.floor(Math.random()*11);
 
-
+console.log (computerSelection);
         
 // computer's options, chosen at random
 function computerPlay () {
-    if (computerSelection <= .33) {
+    if (computerSelection <= 3) {
         return "Rock"
-    } else if (computerSelection <= .67) {
+    } else if (computerSelection <= 6) {
         return "Paper"
     } else {
         return "Scissors"
     }
 }
-
+console.log (computerPlay());
 
 function playRound () {
     if (playerSelection = "rock")
