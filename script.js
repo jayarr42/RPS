ae857c4e2c5b998fe8eb4adee1f114d09a6dcc52
JayArr <jrollock57@gmail.com>
2022-04-13 12:22:14 -0400

    Create rock win/lose/draw conditions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // user input
-//let playerSelection = prompt ("What is your choice?");
+let playerSelection = prompt ("What is your choice?");
 
 // assign variable to the comp select, used in comp play
 let computerSelection = Math.floor(Math.random()*11);
@@ -16,9 +16,16 @@ function computerPlay () {
         return "Scissors"
     }
 }
-console.log (computerPlay());
 
+
+// trying to create a single round of play
 function playRound () {
-    if (playerSelection = "rock")
-    return computerPlay ();
+    if (playerSelection === "rock" & computerPlay() === "Rock") {
+    return "It's a tie!";
+    } else if (playerSelection === "rock" & computerPlay() === "Paper") {
+    return "You lose!";
+    } else if (playerSelection === "rock" & computerPlay() === "Scissors") {
+    return "You win!";
+    }
 }
+console.log(playRound());
